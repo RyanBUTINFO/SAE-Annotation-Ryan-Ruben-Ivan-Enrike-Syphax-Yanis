f566e1b68f3cb97a6d3cc031f401a49c5faaf7dd
Enrike Champion <enrikechampion@gmail.com>
2024-12-12 09:37:49 +0100

    Ajout des commentaires dans les Controllers

--- Controllers/Controller_compte.php
+++ Controllers/Controller_compte.php
@@ -2,7 +2,7 @@
 class Controller_compte extends Controller{
     
     /**
-     * Action par défaut affichant la page d'acceuil.
+     * Action par défaut affichant la page de connexion.
      */
     public function action_default(){
         $m=Model::getModel();
@@ -10,12 +10,18 @@ class Controller_compte extends Controller{
         $this->render("connexion", $data);
     }
 
+    /**
+     * Action affichant la page de création de compte.
+     */
     public function action_creation(){
         $m=Model::getModel();
         $data=[];
         $this->render("creation", $data);
     }
 
+    /**
+     * Action affichant la page de connexion réussie.
+     */
     public function action_succes(){
         $m=Model::getModel();
         $data=[];
--- Controllers/Controller_settings.php
+++ Controllers/Controller_settings.php
@@ -2,6 +2,9 @@
 
 class Controller_settings extends Controller{
 
+    /**
+     * Action par défaut affichant la page des paramètres.
+     */
     public function action_default(){
         $m=Model::getModel();
         $data=[];
@@ -9,6 +12,9 @@ class Controller_settings extends Controller{
 
     }
 
+    /**
+     * Action affichant la page des conditions d'utilisation.
+     */
     public function action_conditions(){
         $m=Model::getModel();
         $data=[];
