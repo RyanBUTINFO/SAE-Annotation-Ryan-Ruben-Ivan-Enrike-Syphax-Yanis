4c5580618599269ddb6c7126dfcd16441e98ae7d
Ruben-Dubord <rubendubord95@gmail.com>
2024-12-19 11:22:32 +0100

    Modification du script de création de la base de données (version finale normalement)

--- BdD/script.sql
+++ BdD/script.sql
@@ -1,28 +1,38 @@
-CREATE TABLE users (
-	id_user SERIAL PRIMARY KEY,
-	email VARCHAR UNIQUE NOT NULL,
-	user_name VARCHAR UNIQUE NOT NULL,
-	passwd VARCHAR NOT NULL,
-);
+CREATE TABLE User(user_id INT PRIMARY KEY,
+username VARCHAR(50) NOT NULL,
+password_hash VARCHAR(255) NOT NULL,
+email VARCHAR(100) NOT NULL,
+created_at TIMESTAMP,
+last_online_at TIMESTAMP);
 
-CREATE TABLE annotation (
-	id_annotation INTEGER PRIMARY KEY,
-	code_hexa VARCHAR UNIQUE NOT NULL,
-	id_emotion INTEGER REFERENCES emotion(id_emotion)
-);
+CREATE TABLE UserStatus(user_id INT PRIMARY KEY,
+is_online BOOLEAN,
+last_active_at TIMESTAMP,
+FOREIGN KEY (user_id) REFERENCES User(user_id));
 
-CREATE TABLE emotion (
-	id_emotion INTEGER PRIMARY KEY,
-	emotion_char VARCHAR UNIQUE NOT NULL
-);
+CREATE TYPE emotions AS ENUM('joie', 'colère', 'tristesse', 'surprise', 'dégoût', 'peur');
 
-CREATE TABLE message_ (
-	id_message SERIAL PRIMARY KEY,
-	message_content VARCHAR NOT NULL,
-	sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	id_sender INTEGER REFERENCES users(id_user),
-	id_recipient INTEGER REFERENCES users(id_user),
-	annotation_sender VARCHAR REFERENCES annotation(code_hexa),
-	annotation_recipient VARCHAR REFERENCES annotation(code_hexa)
-);
+CREATE TABLE Conversation(conversation_id INT PRIMARY KEY,
+user_1_id INT,
+user_2_id INT,
+created_at TIMESTAMP,
+FOREIGN KEY (user_1_id) REFERENCES User(user_id),
+FOREIGN KEY (user_2_id) REFERENCES User(user_id));
 
+CREATE TABLE Messages(message_id INT PRIMARY KEY,
+conversation_id INT,
+sender_id INT,
+receiver_id INT,
+content TEXT NOT NULL,
+created_at TIMESTAMP,
+FOREIGN KEY (conversation_id) REFERENCES Conversation(conversation_id),
+FOREIGN KEY (sender_id) REFERENCES User(user_id),
+FOREIGN KEY (receiver_id) REFERENCES User(user_id));
+
+CREATE TABLE Annotation(annotation_id INT PRIMARY KEY,
+message_id INT,
+annotator_id INT,
+emotion emotions NOT NULL,
+created_at TIMESTAMP,
+FOREIGN KEY (message_id) REFERENCES Messages(message_id),
+FOREIGN KEY (annotator_id) REFERENCES User(user_id));
